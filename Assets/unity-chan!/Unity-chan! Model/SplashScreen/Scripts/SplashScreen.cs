056935ee0071c7257fb2f912a9868cbc561cce3f
Bastien Humeau <bastienh@unity3d.com>
2019-11-16 15:08:55 -0500

    demo

---
+++ Assets/unity-chan!/Unity-chan! Model/SplashScreen/Scripts/SplashScreen.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using System.Collections;
+
+namespace UnityChan
+{
+    [ExecuteInEditMode]
+    public class SplashScreen : MonoBehaviour
+    {
+        void NextLevel()
+        {
+            Application.LoadLevel(Application.loadedLevel + 1);
+        }
+    }
+}
