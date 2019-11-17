2185be1d691e252565136efde2d3d82a41c712c9
Bastien Humeau <bastienh@unity3d.com>
2019-11-17 01:34:10 -0500

    Frame order editor with its own window in scene

--- Packages/com.unity.formats.piskel/Editor/EditorTool/PiskelEditorTool.cs
+++ Packages/com.unity.formats.piskel/Editor/EditorTool/PiskelEditorTool.cs
@@ -65,6 +65,41 @@ namespace Unity.Formats.Piskel.Editor
         {
             // Instantiate the clip, we always want to work on a copy.
             _animation = Instantiate(animation);
+
+            _curveBindings = AnimationUtility.GetObjectReferenceCurveBindings(_animation);
+            _selectedLayerIndex = _selectedLayerIndex >= _curveBindings.Length ? 0 : _selectedLayerIndex;
+            InitializeFramesList();
+        }
+
+        private void InitializeFramesList()
+        {
+            _keyFramesList = AnimationUtility.GetObjectReferenceCurve(_animation, _curveBindings[_selectedLayerIndex]);
+            _spritesListUi = new ReorderableList(_keyFramesList, typeof(ObjectReferenceKeyframe), true, false, false, false);
+            _spritesListUi.headerHeight = 1f;
+            _spritesListUi.footerHeight = 1f;
+            _spritesListUi.drawElementCallback = (rect, index, active, focused) =>
+            {
+                var element = _keyFramesList[index];
+                rect.y += 2f;
+                rect.height = EditorGUIUtility.singleLineHeight;
+                EditorGUI.BeginChangeCheck();
+                element.value = EditorGUI.ObjectField(rect, element.value, typeof(Sprite), false);
+                if (EditorGUI.EndChangeCheck())
+                {
+                    _keyFramesList[index] = element;
+                    AnimationUtility.SetObjectReferenceCurve(_animation, _curveBindings[_selectedLayerIndex], _keyFramesList);
+                    _hasListChanged = true;
+                }
+            };
+            _spritesListUi.onReorderCallback = list =>
+            {
+                for (int i = 0; i < _keyFramesList.Length; i++)
+                {
+                    _keyFramesList[i].time = (1 / _animation.frameRate) * i;
+                }
+                AnimationUtility.SetObjectReferenceCurve(_animation, _curveBindings[_selectedLayerIndex], _keyFramesList);
+                _hasListChanged = true;
+            };
         }
 
         void OnDisable()
@@ -93,8 +128,54 @@ namespace Unity.Formats.Piskel.Editor
             Handles.color = AnimationMode.animatedPropertyColor;
             Handles.DrawWireCube(piskelBound.center, piskelBound.size);
             Handles.color = oldColor;
+
+            var windowCenter = piskelBound.center + piskelBound.size / 2f;
+            windowCenter = sceneview.camera.WorldToScreenPoint(windowCenter);
+            windowCenter.y = sceneview.position.height - windowCenter.y;
+            windowCenter.x += 20f;
+            Handles.BeginGUI();
+            GUILayout.Window(GetInstanceID(), new Rect(windowCenter, new Vector2(200f, 180f)), DrawPiskelEditorToolWindow, "Piskel Editor Tool");
+            Handles.EndGUI();
+
             // Force sceneview.Repaint() to make sure the animation is always playing
             sceneview.Repaint();
+        }
+
+        private ReorderableList _spritesListUi;
+        private ObjectReferenceKeyframe[] _keyFramesList = new ObjectReferenceKeyframe[0];
+        private Vector2 _scrollPosition;
+        private bool _hasListChanged;
+        private int _selectedLayerIndex = 0;
+
+        private void DrawPiskelEditorToolWindow(int id)
+        {
+            EditorGUI.BeginChangeCheck();
+            var newIndex = EditorGUILayout.Popup(_selectedLayerIndex, _curveBindings.Select(i => i.propertyName).ToArray());
+            if (EditorGUI.EndChangeCheck() && newIndex != _selectedLayerIndex)
+            {
+                InitializeFramesList();
+            }
+
+            _scrollPosition = EditorGUILayout.BeginScrollView(_scrollPosition);
+            _spritesListUi.DoLayoutList();
+            EditorGUILayout.EndScrollView();
+
+            EditorGUI.BeginDisabledGroup(!_hasListChanged);
+            if (GUILayout.Button("Save changes"))
+            {
+                string savePath =
+                    EditorUtility.SaveFilePanelInProject("Save Piskel animation", piskelTarget.name, "piskel", "Select where to save the current animation.");
+                if (!string.IsNullOrEmpty(savePath))
+                {
+                    PiskelDocument document = PiskelDocument.FromAnimation(_animation);
+                    File.WriteAllText(savePath, document.ToPiskelJson());
+                    AssetDatabase.ImportAsset(savePath);
+                    _hasListChanged = false;
+                }
+            }
+            EditorGUI.EndDisabledGroup();
+
+            GUILayout.FlexibleSpace();
         }
     }
 }
